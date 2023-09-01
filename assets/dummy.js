36ed7e00e145cdfeb0c4d8c72a39d021055e4df5
mavdavis <ekwesimarvelous@gmail.com>
2023-09-01 18:00:34 +0100

    adding dummy data

---
+++ assets/dummy.js
@@ -0,0 +1,19 @@
+const dummy_data = [
+  {
+    img: "",
+    name: "",
+    kinopoisk_rating: "",
+    imdb_rating: "",
+    genre: "",
+    release_date: "",
+    director: "",
+    cast: "",
+    runtime: "",
+    certificate: "",
+    description: "",
+    Eurasia_Cinema: {
+      available_time: [],
+      distance_from_your_location: "2.4km",
+    },
+  },
+];
